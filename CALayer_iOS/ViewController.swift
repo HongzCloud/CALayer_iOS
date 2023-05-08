f06794f1e90f05812361cb246e143cd93074eaf0
HongzCloud <jihong21511@gmail.com>
2023-05-08 09:49:10 +0900

    :lipstick: Gradient 및 Animation 추가

--- CALayer_iOS/ViewController.swift
+++ CALayer_iOS/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         
         self.view.addSubview(testView)
         addShadow()
+        addGradient()
     }
     
     func addShadow() {
@@ -23,5 +24,24 @@ class ViewController: UIViewController {
         testView.layer.shadowOpacity = 1
         testView.layer.shadowOffset = CGSize(width: 10, height: 10)
         testView.layer.shadowRadius = 2
+    }
+
+    func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = testView.bounds
+        gradientLayer.colors = [UIColor.green.cgColor, UIColor.yellow.cgColor]
+        gradientLayer.borderWidth = 1
+        gradientLayer.borderColor = UIColor.black.cgColor
+
+        testView.layer.addSublayer(gradientLayer)
+
+         let animation = CABasicAnimation(keyPath: "bounds.size.height")
+         animation.fromValue = gradientLayer.bounds.height
+         animation.toValue = gradientLayer.bounds.height * 2
+         animation.duration = 2.0
+         animation.autoreverses = true
+         animation.repeatCount = .infinity
+
+         gradientLayer.add(animation, forKey: "bounds.size.height")
     }
 }
